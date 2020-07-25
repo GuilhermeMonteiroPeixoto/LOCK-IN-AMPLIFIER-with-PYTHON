cb09297a49ad8a4f3ee5cbf4e259da4d2d7afb4a
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2020-07-25 16:13:32 -0300

    Update Espectro_Frequencia.py

--- Outros/Espectro_Frequencia.py
+++ Outros/Espectro_Frequencia.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 data = pd.read_csv('HC-1.csv', header=None) # ou pd.read_csv('out.csv',header=None)
 
-plt.plot(data.iloc[:,0])
+plt.plot(data.iloc[:,0],linewidth=0.2)
 plt.show()
 
 #Metodo 1 - Usando Numpy
@@ -17,7 +17,7 @@ freq = np.fft.fftfreq(tamanho, d=tx)
 mascara = freq > 0
 fft_calculo = np.fft.fft(sinal)
 fft_abs = 2.0*np.abs(fft_calculo/tamanho)
-plt.plot(freq[mascara],fft_abs[mascara])
+plt.plot(freq[mascara],fft_abs[mascara],linewidth=0.2)
 plt.xlim(0,150)
 plt.ylim(0.00000, 0.00006)
 plt.show()
@@ -29,13 +29,13 @@ tx = 0.001
 y = data.iloc[0:,1].to_list()
 yf = fft(y)
 xf = np.linspace(0.0, 1.0/(2.0*tx), int(N/2))
-plt.plot(xf, 2.0/N*np.abs(yf[0:int(N/2)]))
+plt.plot(xf, 2.0/N*np.abs(yf[0:int(N/2)]),linewidth=0.2)
 plt.xlim(0,150)
 plt.ylim(0.00000, 0.00006)
 plt.show()
 
 #Metodo 3 - Usando matplotlib
-plt.magnitude_spectrum(sinal, Fs=1/tx)
+plt.magnitude_spectrum(sinal, Fs=1/tx,linewidth=0.2)
 plt.xlim(0,150)
 plt.ylim(0.00000, 0.00006)
 plt.show()
